47c617b9df243b0b1863280fedfd03ee53904c6a
Leonardo Ostjen <leonardoostjen@gmail.com>
2025-04-02 17:22:13 -0300

    improve position matching

--- src/utils/app_utils.js
+++ src/utils/app_utils.js
@@ -12,10 +12,11 @@ export const get_matches = (player, target_player) => {
     let age_match = player.age === target_player.age ? MatchType.EXACT : (player.age === target_player.age + 1 || player.age === target_player.age - 1) ? MatchType.PARTIAL : MatchType.NONE;
     
     // when a player has the same position as the target it's an exact match, if they play in an alternative_positions array it's a partial match
-    let position_match = player.position === target_player.position ? MatchType.EXACT : player.alternative_positions.includes(target_player.position) ? MatchType.PARTIAL : MatchType.NONE;
+    let position_match = player.position === target_player.position ? MatchType.EXACT : (player.alternative_positions.includes(target_player.position) || target_player.alternative_positions.includes(player.position)) ? MatchType.PARTIAL : MatchType.NONE;
 
-    // TODO: change height data to accomodate a range comparation
-    let height_match = player.height === target_player.height ? MatchType.EXACT : MatchType.NONE;
+    let parsed_height = parseInt(player.height.split('cm')[0]);
+    let target_parsed_height = parseInt(target_player.height.split('cm')[0]);
+    let height_match = parsed_height === target_parsed_height ? MatchType.EXACT : (parsed_height >= target_parsed_height * 0.98 && parsed_height <= target_parsed_height * 1.02) ? MatchType.PARTIAL : MatchType.NONE;
 
     return {
         nation: nation_match,
@@ -25,3 +26,4 @@ export const get_matches = (player, target_player) => {
         height: height_match,
     }
 }
+
