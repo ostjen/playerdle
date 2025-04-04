e9c116459f2688b54f6b1becff62ca3bc979b5a6
Leonardo Ostjen <leonardoostjen@gmail.com>
2025-04-04 17:48:18 -0300

    multilanguage support

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -28,6 +28,9 @@ i18n
     debug: process.env.NODE_ENV === 'development',
     interpolation: {
       escapeValue: false // not needed for react as it escapes by default
+    },
+    react: {
+      useSuspense: false
     }
   });
 
